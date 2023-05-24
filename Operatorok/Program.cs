dca36c105d986f59fa7ccf07b7daa8f58ee21ebe
Sushkoo <szilagyidominik005@gmail.com>
2023-05-24 09:50:18 +0200

    Add project files.

---
+++ Operatorok/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+	internal class Program
+	{
+		static List<Operatorok> lista = new List<Operatorok>();
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
